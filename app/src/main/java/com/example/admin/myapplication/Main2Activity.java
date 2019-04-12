8764ceca7f7d36640228c95ddce6982ab5076a7e
admin <386392068@qq.com>
2019-04-12 09:25:19 +0800

    第四次提交

--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -14,26 +20,35 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class Main2Activity extends AppCompatActivity {
+    private String[] data={"北京","浙江","温州"};
     private TextView textView;
-    private Button button;
+    private ListView listview;
+
+//    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         this.textView=(TextView) findViewById(R.id.abcd);
-        this.button = (Button)findViewById(R.id.button1);
-        this.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Main2Activity.this,MainActivity.class));
-            }
-        });
+        this.listview=(ListView) findViewById(R.id.listview);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+        listview.setAdapter(adapter);
+
+//        this.button = (Button)findViewById(R.id.button1);
+//        this.button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(Main2Activity.this,MainActivity.class));
+//            }
+//        });
 
         String weatherUrl = "http://guolin.tech/api/china";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
+                String[] result=parseJSONObject(responseText);
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -46,5 +61,30 @@ public class Main2Activity extends AppCompatActivity {
 
             }
         });
+    }
+    private String[] parseJSONObject(String responseText){
+        JSONArray jsonArray= null;
+        try{
+            jsonArray=new JSONArray(responseText);
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        String[] result = new String[jsonArray.length()];
+        for (int i = 0;i < jsonArray.length();i++){
+            JSONObject jsonObject= null;
+            try {
+                jsonObject=jsonArray.getJSONObject(i);
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+           try {
+                result[i] = jsonObject.getString("name");
+           }catch (JSONException e){
+                e.printStackTrace();
+           }
+        }
+        return null;
+
     }
 }
