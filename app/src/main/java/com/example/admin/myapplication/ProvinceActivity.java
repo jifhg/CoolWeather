51b6729ab59f49089ee22115136857ab9ea5c8a5
admin <386392068@qq.com>
2019-05-10 14:51:52 +0800

    第八次提交

--- app/src/main/java/com/example/admin/myapplication/ProvinceActivity.java
+++ app/src/main/java/com/example/admin/myapplication/ProvinceActivity.java
@@ -1,14 +1,13 @@
 package com.example.admin.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,88 +22,78 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class ProvinceActivity extends AppCompatActivity {
-
-    private String currentlevel = "province";
-    private int pid = 0;
+    public static final String PROVINCE = "province";
+    public static final String CITY = "city";
+    public static final String COUNTY = "county";
+    private String currentlevel =PROVINCE;
+    private int provinceId = 0;
+    private int cityId=0;
+    private int weatherId=0;
     private List<Integer> pids = new ArrayList<>();
     private List<String> data = new ArrayList<>();
-
-    private ListView listView;
-
-
+    private List<String> weatherIds=new ArrayList<>();
+    private ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        //TODO if(currentlevel=="city"){
-        // Intent intent=getIntent();
-        // final int pid = intent.getIntExtra("pid",0);
-        // }
-
-
-        this.listView = (ListView) findViewById(R.id.list_view);
+        this.listview = (ListView) findViewById(R.id.list_view);
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
-        listView.setAdapter(adapter);
-        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listview.setAdapter(adapter);
+        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.i("点击了哪一个", "" + position + ":" + ProvinceActivity.this.pids.get(position) + ":" + ProvinceActivity.this.data.get(position));
-                pid = ProvinceActivity.this.pids.get(position);
-                currentlevel = "city";
+
+                if(currentlevel== PROVINCE){
+                    currentlevel = CITY;
+                    provinceId = ProvinceActivity.this.pids.get(position);
+                }else if(currentlevel==CITY){
+                    currentlevel = COUNTY;
+                    cityId = ProvinceActivity.this.pids.get(position);
+                }else if(currentlevel==COUNTY){
+                    String weatherId=ProvinceActivity.this.weatherIds.get(position);
+
+                    Intent intent= new Intent(ProvinceActivity.this,WeatherActivity.class);
+                    intent.putExtra("wid",weatherId);
+                    startActivity(intent);
+
+                }
+
                 getData(adapter);
-
-
-//                Intent intent = new Intent(ProvinceActivity.this,CityActivity.class);
-//                intent.putExtra("pid",ProvinceActivity.this.pids[position]);
-//                if (currentlevel == "city"){
-//                    intent.putExtra("cid",cids[position]);
-//                }
-//                startActivity(intent);
             }
         });
-
-
         getData(adapter);
+
     }
 
     private void getData(final ArrayAdapter<String> adapter) {
-        String weatherUrl = currentlevel == "city" ? "http://guolin.tech/api/china/" + pid : "http://guolin.tech/api/china/";
-        //String weatherUrl = "http://guolin.tech/api/china";
+        String weatherUrl = currentlevel == PROVINCE ? "http://guolin.tech/api/china":(currentlevel==CITY?"http://guolin.tech/api/china/" + provinceId :"http://guolin.tech/api/china/" + provinceId+"/"+cityId);
+        //    String weatherUrl = "http://guolin.tech/api/china";
         com.example.admin.helloworld1.HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
-
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
-
                 parseJSONObject(responseText);
-
-
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-
-                        //textView.setText(responseText);
                         adapter.notifyDataSetChanged();
-
                     }
                 });
-
             }
 
-
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
         });
     }
-
     private void parseJSONObject(String responseText) {
         JSONArray jsonArray = null;
         this.data.clear();
         this.pids.clear();
+        this.weatherIds.clear();
         try {
             jsonArray = new JSONArray(responseText);
             for (int i = 0; i < jsonArray.length(); i++) {
@@ -112,12 +101,12 @@ public class ProvinceActivity extends AppCompatActivity {
                 jsonObject = jsonArray.getJSONObject(i);
                 this.data.add(jsonObject.getString("name"));
                 this.pids.add(jsonObject.getInt("id"));
-
-
+                if(jsonObject.has("weather_id")){
+                    this.weatherIds.add(jsonObject.getString("weather_id"));
+                }
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 }
-
